dc24fe53ee44158d43a5d94dde60a79009dba5fb
Gerald <geraldalfaro768@gmail.com>
2024-11-12 17:10:30 -0300

    agregado de sub páginas para las secciones de nosotros, pero avance solo en los voluntarios

--- nosotros.php
+++ nosotros.php
@@ -25,25 +25,25 @@ Template Name: Nosotros
     <section class="nosotros-links">
         <div class="container">
             <div class="row">
-                <a href="" class="col-md-3 border-voluntario espaciado-responsive">
+                <a href="<?php echo get_permalink(get_page_by_path('nosotros/voluntarios-de-la-compania')); ?>" class="col-md-3 border-voluntario espaciado-responsive">
                         <img class="link-nosotros-normal" src="<?php echo get_theme_file_uri("img/nosotros-voluntarios-azul.png") ?>" alt="voluntarios azul">
                         <img class="link-nosotros-hover" src="<?php echo get_theme_file_uri("img/nosotros-voluntarios-rojo.png") ?>" alt="voluntarios rojo">
                         <img class="link-nosotros-active" src="<?php echo get_theme_file_uri("img/nosotros-voluntarios-blanco.png") ?>" alt="voluntarios-blanco">
                         <h3 class="mt-3">Voluntarios de la Compañía</h3>
                 </a>
-                <a href="" class="col-md-3">
+                <a href="<?php echo get_permalink(get_page_by_path('nosotros/brigada-juvenil')); ?>" class="col-md-3">
                         <img class="link-nosotros-normal espaciado-responsive" src="<?php echo get_theme_file_uri("img/nosotros-brigada-azul.png") ?>" alt="brigada juvenil azul">
                         <img class="link-nosotros-hover" src="<?php echo get_theme_file_uri("img/nosotros-brigada-rojo.png") ?>" alt="brigada juvenil rojo">
                         <img class="link-nosotros-active" src="<?php echo get_theme_file_uri("img/nosotros-brigada-blanco.png") ?>" alt="brigada juvenil blanco">
                         <h3 class="mt-3">Brigada Juvenil</h3>
                 </a>
-                <a href="" class="col-md-3">
+                <a href="<?php echo get_permalink(get_page_by_path('nosotros/casino-la-primera')); ?>" class="col-md-3">
                         <img class="link-nosotros-normal espaciado-responsive" src="<?php echo get_theme_file_uri("img/nosotros-casino-azul.png") ?>" alt="casino azul">
                         <img class="link-nosotros-hover" src="<?php echo get_theme_file_uri("img/nosotros-casino-rojo.png") ?>" alt="casino rojo">
                         <img class="link-nosotros-active" src="<?php echo get_theme_file_uri("img/nosotros-casino-blanco.png") ?>" alt="casino blanco">
                         <h3 class="mt-3">Casino “La Primera”</h3>
                 </a>
-                <a href="" class="col-md-3 border-centro">
+                <a href="<?php echo get_permalink(get_page_by_path('nosotros/centro-cultural')); ?>" class="col-md-3 border-centro">
                         <img class="link-nosotros-normal" src="<?php echo get_theme_file_uri("img/nosotros-centro-azul.png") ?>" alt="centro azul">
                         <img class="link-nosotros-hover" src="<?php echo get_theme_file_uri("img/nosotros-centro-rojo.png") ?>" alt="centro rojo">
                         <img class="link-nosotros-active" src="<?php echo get_theme_file_uri("img/nosotros-centro-blanco.png") ?>" alt="centro blanco">
---
+++ page-nosotros-secciones.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Template Name: Nosotros Secciones
+*/
+?>
+
+<?php get_header(); ?>
+
+<main>
+    <div class="secciones-nosotros">
+        <!-- Condicionales -->
+        <?php if (is_page('voluntarios-de-la-compania')): ?>
+            <!-- Estructura específica para "Voluntarios de la Compañía" -->
+            <section class="bg-voluntarios">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h1>Voluntarios compañía</h1>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="buscador">
+                                <input type="text" id="buscador" placeholder="Buscar...">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="container mt-5">
+                    <div class="col-md-10 mx-auto seccion-voluntarios-tab">
+                        <div class="tab-boton-voluntarios">
+                            <input type="radio" id="humano" name="tabs" checked>
+                            <label class="tab" for="humano">Humano</label>
+                            <input type="radio" id="animal" name="tabs">
+                            <label class="tab" for="animal">Animal</label>
+                            <span class="slider"></span>
+                        </div>
+                    </div>             
+            </section>
+        <?php elseif (is_page('brigada-juvenil')): ?>
+            <!-- Estructura específica para "Brigada Juvenil" -->
+            <p>Contenido para Brigada Juvenil.</p>
+        <?php elseif (is_page('casino-la-primera')): ?>
+            <!-- Estructura específica para "Casino La Primera" -->
+            <p>Contenido para Casino La Primera.</p>
+        <?php elseif (is_page('centro-cultural')): ?>
+            <!-- Estructura específica para "Centro Cultural" -->
+            <p>Contenido para Centro Cultural.</p>
+        <?php endif; ?>
+    </div>
+</main>
+
+<?php get_footer(); ?>
